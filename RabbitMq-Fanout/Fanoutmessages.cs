79b8d20433b232b62343a00d6d2026b867fa00ed
luisg08 <luisgust08@univem.edu.br>
2020-11-14 00:06:42 -0300

    Commit

---
+++ RabbitMq-Fanout/Fanoutmessages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RequestRabbitMQ
+{
+    public class Fanoutmessages
+    {
+        /*private const string UName = "guest";
+        private const string PWD = "guest";
+        private const string HName = "localhost";*/
+
+        public void SendMessage()
+        {
+            //Main entry point to the RabbitMQ .NET AMQP client
+            var connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Uri = new Uri("amqps://ucrhweyl:eYCN6txHd4NPy_blBoKJIwKo1apxm3E5@jackal.rmq.cloudamqp.com/ucrhweyl");
+            connectionFactory.AutomaticRecoveryEnabled = true;
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            var properties = model.CreateBasicProperties();
+
+            properties.Persistent = false;
+            byte[] messagebuffer = Encoding.Default.GetBytes("Mensagem para exchange do tipo Fanout");
+
+            model.BasicPublish("fanout.exchange", "", properties, messagebuffer);
+            Console.WriteLine("\nRabbitMq Fanout.");
+            Console.WriteLine("Mensagem enviada para todas as filas");
+        }
+    }
+}
---
+++ RabbitMq-Fanout/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RequestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            Fanoutmessages fanoutmessages = new Fanoutmessages();
+            do
+            {
+                Console.WriteLine("\n\nEnviar mensagem?");
+                opt = Console.ReadLine();
+                if ((opt == "Sim") || (opt == "sim"))
+                    fanoutmessages.SendMessage();
+                else
+                    Environment.Exit(0);
+            } while (true);
+            
+            Console.ReadLine();
+        }
+    }
+}
